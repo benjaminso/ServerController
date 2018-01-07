7cfd8497567037927fe5fed9a5b706e81d720838
HieuSo <HieuSo@DESKTOP-M645SEF>
2018-01-06 17:35:19 -0800

    testing GUI

--- LocalServerController/ServerControl/src/application/view/CenterMenuController.java
+++ LocalServerController/ServerControl/src/application/view/CenterMenuController.java
@@ -1,17 +1,21 @@
 package application.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 
+import java.awt.TextField;
 import java.awt.event.ActionListener;
 
 public class CenterMenuController {
 	@FXML
-	
 	private MenuItem item1;
 	private MenuItem item2;
+	private TextField txtServername;
+	private Button btnSubmit;
 	private Label lbText;
+	
 
 	public void item1Action() {
 			System.out.println("asdasdasds");
@@ -19,6 +23,9 @@ public class CenterMenuController {
 	}
 	public void item2Action() {
 		System.out.println("mothaiba");
+	}
+	public void submitAction() {
+		System.out.println("submit");
 	}
 
 }
