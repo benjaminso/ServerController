ea7cd522c274de9f01ae198fa68d631fc5f12ad1
HieuSo <HieuSo@DESKTOP-M645SEF>
2018-01-06 18:03:49 -0800

    Controlling Classes

--- LocalServerController/ServerControl/src/application/view/CenterMenuController.java
+++ LocalServerController/ServerControl/src/application/view/CenterMenuController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.MenuItem;
 
 import java.awt.TextField;
 import java.awt.event.ActionListener;
+import application.BusinessRules.ControllingInput;;
 
 public class CenterMenuController {
 	@FXML
@@ -15,17 +16,16 @@ public class CenterMenuController {
 	private TextField txtServername;
 	private Button btnSubmit;
 	private Label lbText;
-	
+	ControllingInput validation=new ControllingInput();
 
 	public void item1Action() {
 			System.out.println("asdasdasds");
-			
 	}
 	public void item2Action() {
 		System.out.println("mothaiba");
 	}
 	public void submitAction() {
 		System.out.println("submit");
+		validation.printDemo();
 	}
-
 }
