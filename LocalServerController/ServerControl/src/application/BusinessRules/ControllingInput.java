0ee360147fe5e676601b65df958b80e10672d69e
HieuSo <HieuSo@DESKTOP-M645SEF>
2018-01-06 22:38:56 -0800

    validation

--- LocalServerController/ServerControl/src/application/BusinessRules/ControllingInput.java
+++ LocalServerController/ServerControl/src/application/BusinessRules/ControllingInput.java
@@ -9,4 +9,21 @@ public class ControllingInput {
 	public void printDemo() {
 		System.out.println("asdsad");
 	}
+	
+	public static void ValidateServerName(String serverName) {
+		if(serverName==null || serverName.equals(""))
+			throw new IllegalArgumentException("Error!ServerName");
+	}
+	public static void ValidateServerCapacity(String serverCapacity) {
+		if(serverCapacity==null || serverCapacity.equals(""))
+			throw new IllegalArgumentException("Error!Server Capacity");
+	}
+	public static void ValidateServerOS(String serverOS) {
+		if(serverOS==null || serverOS.equals(""))
+			throw new IllegalArgumentException("Error!Server OS");
+	}
+	public static void ValidateServerAccount(String serverACcount) {
+		if(serverACcount==null || serverACcount.equals(""))
+			throw new IllegalArgumentException("Error!Server Account");
+	}
 }
