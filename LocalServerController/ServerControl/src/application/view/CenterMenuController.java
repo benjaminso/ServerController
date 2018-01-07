88f22c7d8fd62d00845677f708e972f1b12b51c7
HieuSo <HieuSo@DESKTOP-M645SEF>
2018-01-06 22:39:30 -0800

    validation

--- LocalServerController/ServerControl/src/application/view/CenterMenuController.java
+++ LocalServerController/ServerControl/src/application/view/CenterMenuController.java
@@ -4,28 +4,73 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
-
-import java.awt.TextField;
-import java.awt.event.ActionListener;
+import javafx.scene.control.TextField;
 import application.BusinessRules.ControllingInput;;
 
 public class CenterMenuController {
-	@FXML
-	private MenuItem item1;
-	private MenuItem item2;
-	private TextField txtServername;
-	private Button btnSubmit;
-	private Label lbText;
+
+	@FXML private MenuItem item1;
+	@FXML private MenuItem item2;
+	@FXML private TextField txtServername,txtServerCapacity,txtServerOS,txtServerAccounts;
+	@FXML private Button btnSubmit,btnClear;
+	//@FXML private Label lbServerCapacity,lbAccount,lbOS,lbServerName;
+	@FXML private Label lbAlertServer,lbAlertCapacity,lbAlertOS,lbAlertAccount;
+	private static final String NASSIGN=null;
 	ControllingInput validation=new ControllingInput();
 
 	public void item1Action() {
-			System.out.println("asdasdasds");
+		System.out.println("asdasdasds");
 	}
 	public void item2Action() {
 		System.out.println("mothaiba");
 	}
 	public void submitAction() {
-		System.out.println("submit");
-		validation.printDemo();
+		// Server Name validation
+		try {
+			ControllingInput.ValidateServerName(txtServername.getText());
+		}catch(IllegalArgumentException e) {
+			if(e!=null) {
+				//System.out.println("asdasdasd");
+				lbAlertServer.setText("*");
+			}
+		}
+		// ValidateServerCapacity
+		try {
+			ControllingInput.ValidateServerCapacity(txtServerCapacity.getText());
+		}catch(IllegalArgumentException e) {
+			if(e!=null) {
+				//System.out.println("ValidateServerCapacity");
+				lbAlertCapacity.setText("*");
+			}
+		}
+		//	ValidateServerOS
+		try {
+			ControllingInput.ValidateServerOS(txtServerOS.getText());
+		}catch(IllegalArgumentException e) {
+			if(e!=null) {
+				//System.out.println("ValidateServerOS");
+				lbAlertOS.setText("*");
+			}
+		}
+		//ValidateServerAccount
+		try {
+			ControllingInput.ValidateServerAccount(txtServerAccounts.getText());
+		}catch(IllegalArgumentException e) {
+			if(e!=null) {
+				//System.out.println("ValidateServerAccount");
+				lbAlertAccount.setText("*");
+			}
+		}
+	}
+	public void ClearAction() {
+		lbAlertServer.setText(NASSIGN);
+		lbAlertCapacity.setText(NASSIGN);
+		lbAlertOS.setText(NASSIGN);
+		lbAlertAccount.setText(NASSIGN);
+
+		txtServername.setText(NASSIGN);
+		txtServerCapacity.setText(NASSIGN);
+		txtServerOS.setText(NASSIGN);
+		txtServerAccounts.setText(NASSIGN);
 	}
 }
