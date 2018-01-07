38a7174b09f9af83b7db60597706252854eb8480
HieuSo <HieuSo@DESKTOP-M645SEF>
2018-01-07 08:20:44 -0800

    NA

--- LocalServerController/ServerControl/src/application/BusinessRules/ServerController.java
+++ LocalServerController/ServerControl/src/application/BusinessRules/ServerController.java
@@ -1,6 +1,14 @@
 package application.BusinessRules;
 
 public abstract class ServerController {
-
-	
+	private String serverName;
+	private String serverCapacity;
+	private String serverOS;
+	private String serverAccount;
+	public ServerController(String serverName, String serverCapacity, String serverOS,String serverAccount) {
+		this.serverName=serverName;
+		this.serverCapacity=serverCapacity;
+		this.serverOS=serverOS;
+		this.serverAccount=serverAccount;
+	}
 }
